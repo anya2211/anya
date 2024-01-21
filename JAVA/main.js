5af5797c4b0231ae85b90de14beccd86009b4e84
anya2211 <151422013+anya2211@users.noreply.github.com>
2024-01-21 10:00:34 +0500

    Add files via upload

---
+++ JAVA/main.js
@@ -0,0 +1,7 @@
+$toggleClassNew('.btn-1', '.chupa', 'active')
+$toggleClassNew('.btn-2', '.dasha', 'active2')
+$toggleClassNew('.btn-3', '.artur', 'active3')
+
+$toggleClassNew('.btn-1','.section-1','chupa-2' )
+$toggleClassNew('.btn-2','.section-2','chupa-3' )
+$toggleClassNew('.btn-3','.section-3','chupa-4' )
