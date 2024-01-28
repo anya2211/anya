4b43e7237253a67ef11cdfeac2247635114fa0f1
anya2211 <151422013+anya2211@users.noreply.github.com>
2024-01-28 13:41:51 +0500

    Add files via upload

---
+++ anya_java/main.js
@@ -0,0 +1,44 @@
+
+
+// let isStudent
+// isStudent = true
+
+// console.log(isStudent);
+
+// isStudent = false
+
+// console.log(isStudent);
+
+// let a = 5
+// let b = 10
+
+// if(a < b){
+//     console.log('привет');
+// }else{
+//     console.log('пока');
+// }
+
+
+
+
+
+// if(vopros > 18 ){
+    //     alert ('вам доступен сайт');
+    // }else{
+        //     alert('не получилось')
+        // }
+        
+        // console.log(vopros);
+        
+        
+        
+        
+        let vopros = prompt('сколько вышел счет?')
+
+if (vopros > 50 && vopros <100 ){
+    alert('дорогой ресторан')
+}else{
+    alert('для бомжей')
+}
+
+
