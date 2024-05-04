dd2dd66e4885a454c0fa43c27b660f08709f5fe0
anya2211 <151422013+anya2211@users.noreply.github.com>
2024-05-04 14:35:47 +0500

    Add files via upload

--- java_final/script.js
+++ java_final/script.js
@@ -1,6 +1,13 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
+const link = document.querySelector(".lnk")
+console.log(link);
 
 burger.addEventListener("click", () => {
   menu.classList.toggle("open");
+});
+
+
+link.addEventListener("click", () => {
+  menu.classList.remove("open");
 });
